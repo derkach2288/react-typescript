bfbb72eb22a72a4fe0d33672817a6b23f8c76eb7
derkach2288 <derkach2288@gmail.com>
2023-11-29 22:35:30 +0100

    add homework20

--- src/homeworks/Homework20/Homework20.tsx
+++ src/homeworks/Homework20/Homework20.tsx
@@ -1,7 +1,23 @@
-import "./styles.css"
+import "./styles.css";
 
 function Homework20() {
-  return <div className="homework20-erapper">hi</div>;
+  let firstName: string = "John";
+  let lastName: string = "Doe";
+  let job: string = "Software Engineer";
+  let isManager: boolean = false;
+  let experience: number = 5;
+  let email: string = "john.doe@google.com";
+
+  return (
+    <div className="homework20-wrapper">
+      <div>{firstName}</div>
+      <div>{lastName}</div>
+      <div>{job}</div>
+      <div>{isManager ? "Manager" : "Employee"}</div>
+      <div>{experience}</div>
+      <div>{email}</div>
+    </div>
+  );
 }
 
 export default Homework20;
