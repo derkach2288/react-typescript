2ab649a5a1d6c26ac9f8e44d2a0fc1405691280d
derkach2288 <derkach2288@gmail.com>
2023-11-29 21:49:46 +0100

    add homework20

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Lesson20 from "./lessons/Lesson20";
+import Homework20 from "./homeworks/Homework20";
 
 function App() {
   return (
     <div className="App">
-      <Lesson20 />
+      {/* <Lesson20 /> */}
+      <Homework20 />
     </div>
   );
 }
---
+++ src/homeworks/Homework20/Homework20.tsx
@@ -0,0 +1,7 @@
+import "./styles.css"
+
+function Homework20() {
+  return <div className="homework20-erapper">hi</div>;
+}
+
+export default Homework20;
