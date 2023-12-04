9f3288604a2e630239537e7579f8ba581929ad56
derkach2288 <derkach2288@gmail.com>
2023-12-04 15:48:15 +0100

    initiak hw22

---
+++ src/homeworks/Homework22/Homework22.tsx
@@ -0,0 +1,15 @@
+import { CSSProperties } from "react";
+import Button from "components/Button";
+import { Homework22Wrapper } from "./styles";
+
+function Homework22() {
+  return (
+    <Homework22Wrapper>
+      <div>
+        <Button name="LogIn" onClick={() => {}} />
+      </div>
+    </Homework22Wrapper>
+  );
+}
+
+export default Homework22;
---
+++ src/homeworks/Homework22/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Homework22Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #0e243b;
+`;
