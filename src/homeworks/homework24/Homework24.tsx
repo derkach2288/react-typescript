22b4c9eabd4652e52e03f20d1579f781f0cfb56d
derkach2288 <derkach2288@gmail.com>
2023-12-06 21:32:37 +0100

    add hw24

--- src/homeworks/homework24/Homework24.tsx
+++ src/homeworks/homework24/Homework24.tsx
@@ -1,7 +1,64 @@
+import { useState, useEffect, ChangeEvent } from "react";
+
+import Button from "components/Button";
+
+import { Homework24Wrapper, Text, Card, ContainerJokes } from "./styles";
+
+function Homework24() {
+  const [jokeQuestion, setJokeQuestion] = useState<string | undefined>(
+    undefined
+  );
+  const [jokeAnswer, setJokeAnswer] = useState<string | undefined>(undefined);
+  const [jokeError, setJokeError] = useState<string | undefined>(undefined);
+
+  const getJokes = async () => {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    console.log(response);
+
+    if (response.ok) {
+      const data = await response.json();
+      // console.log(data);
+      setJokeQuestion(data.setup);
+      setJokeAnswer(data.punchline);
+    } else {
+      setJokeError("Ошибка при получении данных");
+
+    }
+  };
+
+  const handlerUpdateJoke = () => {
+    if(jokeError === undefined) {
+      alert("Вы получили новую шутку");
+      getJokes();
+    } else {
+      alert("Ошибка при получении данных")
+    }
+  };
 
 
-function Homework24 () {
-  return (<div>www</div>);
+  useEffect(() => {
+    getJokes();
+  }, []);
+
+  return (
+    <Homework24Wrapper>
+      <Card>
+        <ContainerJokes>
+          {jokeQuestion && <Text>{jokeQuestion}</Text>}
+          {jokeAnswer && <Text>{jokeAnswer}</Text>}
+          {jokeError && <Text>{jokeError}</Text>}
+        </ContainerJokes>
+        <Button
+          name="new joke"
+          onClick={() => {
+            handlerUpdateJoke();
+          }}
+        />
+      </Card>
+    </Homework24Wrapper>
+  );
 }
 
 export default Homework24;
--- src/homeworks/homework24/styles.ts
+++ src/homeworks/homework24/styles.ts
@@ -1,12 +1,12 @@
-import {styled} from "styled-components"
+import { styled } from "styled-components";
 
-import { colors } from "styles/colors"
+import { colors } from "styles/colors";
 
-export const Lesson24Wrapper = styled.div`
+export const Homework24Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 70px;
+  padding: 200px;
   width: 100%;
   height: 100%;
   gap: 100px;
@@ -16,14 +16,26 @@ export const Lesson24Wrapper = styled.div`
 export const Text = styled.p`
   font-size: 20px;
   color: black;
+  text-align: center;
 `;
 
 export const Card = styled.div`
   display: flex;
+  justify-content: space-between;
   flex-direction: column;
-  gap:10px;
+  gap: 10px;
   width: 100%;
   height: 500px;
   padding: 10px;
   background-color: white;
+  border-radius: 8px;
+`;
+
+export const ContainerJokes = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  height: 80%;
 `;
